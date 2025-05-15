b7dcbd3edb6d9e72388e05408bb9c609919f9a9f
Jaydee White <jddubble2@gmail.com>
2025-05-15 13:12:05 -0600

    Pushed new project without video

--- Back-End/main.ts
+++ Back-End/main.ts
@@ -32,7 +32,7 @@ console.log(`Listening on port 3005`);
 
 
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({ //Conects to the database
   host: 'localhost',
   user: 'web',
   password: 'AGoodPassword',
@@ -42,7 +42,7 @@ connection.connect((err)=>{
   if (err) console.error(err)
 })
 
-async function hashString(input: string): Promise<string> {
+async function hashString(input: string): Promise<string> { // Hashes strings
   const encoder = new TextEncoder();
   const data = encoder.encode(input); // Uint8Array
 
@@ -54,40 +54,26 @@ async function hashString(input: string): Promise<string> {
 }
 
 app.get("/", ( _ , response)=>{
-  connection.connect((err)=>{
-    if (err) console.error(err)
-  })
-  
-  // connection.query(`SELECT * FROM Meeting`, (err, result)=>{
-  //   if (err) throw err
-  //   console.log(result);
-    
-  // })
-  response.send("Welcome to our Route")
+  response.send("Empty Route")
 })
 
 
 app.post("/removeAdmin", async( req , response)=>{
-  const {removeAddr, adminAddr} = req.body
+  const {removeAddr, adminAddr} = req.body // Retrieves the address being removed and the address trying
+  // v-- Hashes both of them
   const removeHash = hashString(removeAddr)
   const adminHash = hashString(adminAddr)
-  console.log('rand');
   
-  console.log(await contract.removeAdmin(removeHash,adminHash))
+  console.log(await contract.removeAdmin(removeHash,adminHash)) // Runs the contract to remove an admin
   response.send("Ran")
 })
 
 
-app.get("/getAdminNameList", async( _ , response)=>{
-  console.log("Getting admins");
-  
+app.get("/getAdminNameList", async( _ , response)=>{ // Calls the contract function to get all names and returns it
   const list = await contract.getAllAdminNames()
-
-  
   response.send(list)
 })
 app.get("/getMeetingPlaces", ( _ , response)=>{
-  
   const connection = mysql.createConnection({
     host: 'localhost',
     user: 'web',
@@ -99,7 +85,7 @@ app.get("/getMeetingPlaces", ( _ , response)=>{
     if (err) console.error(err)
   })
   
-  connection.query(`SELECT * FROM Meeting`, (err, result)=>{
+  connection.query(`SELECT * FROM Meeting`, (err, result)=>{ // Simply gets all meetings in the database
     if (err) throw err
     console.log(result);
     response.send(result)
@@ -107,26 +93,18 @@ app.get("/getMeetingPlaces", ( _ , response)=>{
   
 })
 app.post("/addAdmin", async( req , response)=>{
-  const {newName, newAddr, adderAddr} = req.body
-  console.log("new name", newName);
-  console.log("new Address", newAddr);
-  console.log("Old Addrss", adderAddr);
-  const newAddrHash = await hashString(newAddr)
+  const {newName, newAddr, adderAddr} = req.body // Gets info
+  const newAddrHash = await hashString(newAddr) // Hashes both ---v
   const oldAddrHash = await hashString(adderAddr)
-  console.log(newAddrHash, "Added Hash");
-  console.log(oldAddrHash, "Sender Hash");
-  
-  
-  
-  await contract.addAdmin(newAddrHash, oldAddrHash, newName)
+  await contract.addAdmin(newAddrHash, oldAddrHash, newName) // Runs the contract to add an admin
   response.send("Ran")
 })
 app.post("/addMeeting", ( req , response)=>{
-  const {host, timeStart, dateStart, timeEnd, address} = req.body
-  const query = `INSERT INTO Meeting (host, timeStart, dateStart, timeEnd, address) VALUES (?, ?, ?, ?, ?)`;
-  const values = [host, timeStart, dateStart, timeEnd, address];
+  const {host, timeStart, dateStart, timeEnd, address} = req.body // Gets info
+  const query = `INSERT INTO Meeting (host, timeStart, dateStart, timeEnd, address) VALUES (?, ?, ?, ?, ?)`; // Sets the query
+  const values = [host, timeStart, dateStart, timeEnd, address]; // Inputs the values to be safe from injections
 
-connection.query(query, values, (err, results) => {
+connection.query(query, values, (err, results) => { 
   if (err) {
     console.error("Database error:", err);
     // handle error
@@ -135,17 +113,15 @@ connection.query(query, values, (err, results) => {
     // handle success
   }
 });
-
     response.send("Welcome to our Route")
 })
 
 app.post("/addMeeting", ( req , response)=>{
   let {host, timeStart, dateStart, timeEnd, address} = req.body
-  console.log("Request body:", req.body);  
-  if (timeEnd === null || timeEnd === undefined){    
+  if (timeEnd === null || timeEnd === undefined){ // Fills timeEnd if it wasn't inputed
     timeEnd = ""
   }
-  if (!host || !timeStart || !dateStart || !address) {
+  if (!host || !timeStart || !dateStart || !address) { // Requires these four 
     response.send("Missing Required Fields")
     return
   }
@@ -158,21 +134,15 @@ app.post("/addMeeting", ( req , response)=>{
     })
     
 })
-app.post("/verifyAdmin", async( req , response)=>{
+app.post("/verifyAdmin", async( req , response)=>{ // Returns a true or false
   const {checkName} = req.body
-  console.log(checkName);
   let found = false
   const list = await contract.getAllAdminNames()
-  console.log(list);
-  
-  for (let i = 0; i < list.length; i++) {
-
+  for (let i = 0; i < list.length; i++) { // Loops through all the names and sees if the passed in name matches
     if (list[i] == checkName){
       found = true
       response.send(true)
     }
-    
   }
   if (!found) response.send(false)
-  
 })
